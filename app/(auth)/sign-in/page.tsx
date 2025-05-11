7562559719a3d6ca17097076e2a4c1f3b843dd05
Harshit Aditya <harshitaditya27@gmail.com>
2025-05-11 19:08:41 +0530

    Added Authentication component and styling

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,84 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <main className="sign-in">
+      <aside className="testimonial">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={32}
+            height={32}
+          />
+          <h1>Screen Recorder App</h1>
+        </Link>
+        <div className="description">
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Image
+                  src="/assets/icons/star.svg"
+                  alt="star"
+                  width={20}
+                  height={20}
+                  key={index}
+                />
+              ))}
+            </figure>
+            <p>
+              I love this screen recorder! It's so easy to use and the quality
+              is amazing. I highly recommend it to anyone who needs to record
+              their screen.
+            </p>
+            <article>
+              <Image
+                src="/assets/images/jason.png"
+                alt="json"
+                width={64}
+                height={64}
+                className="rounded-full"
+              />
+              <div>
+                <h2>Json Statham</h2>
+                <p>Staff Enginner, Oxx Technologies</p>
+              </div>
+            </article>
+          </section>
+        </div>
+        <p> Screen Recorder App {new Date().getFullYear()}</p>
+      </aside>
+      <aside className="google-sign-in">
+        <section>
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="logo"
+              width={40}
+              height={40}
+            />
+            <h1>Screen Recorder App</h1>
+          </Link>
+          <p>
+            Create and share you very first <span> Screen Recorder App</span> in
+            no time !
+          </p>
+          <button>
+            <Image
+              src="/assets/icons/google.svg"
+              alt="google"
+              width={22}
+              height={22}
+            />
+            <span>Sign in with Google</span>
+          </button>
+        </section>
+      </aside>
+      <div className="overlay" />
+    </main>
+  );
 }
 
 export default Page;
