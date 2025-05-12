1abf604e14b1a2c04fd6ef3825504dce88ce3af5
Harshit Aditya <harshitaditya27@gmail.com>
2025-05-12 09:04:14 +0530

    Added upload video form component

---
+++ components/FormField.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function FormField({
+  id,
+  label,
+  type = "text",
+  value,
+  onChange,
+  placeholder,
+  as = "input",
+  options = [],
+}: FormFieldProps) {
+  return (
+    <div className="form-field">
+      <label htmlFor={id}>{label}</label>
+      {as == "textarea" ? (
+        <textarea
+          id={id}
+          name={id}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+      ) : as == "select" ? (
+        <select id={id} name={id} value={value} onChange={onChange}>
+          {options.map(({ label, value }) => (
+            <option key={label} value={value}>
+              {label}{" "}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input id={id} name={id} value={value} onChange={onChange} />
+      )}
+    </div>
+  );
+}
+
+export default FormField;
